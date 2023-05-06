c665cfaa4569a45e5c1b57cb18d1f6109f53a998
yitjhy@126.com <yitjhy@126.com>
2023-05-06 12:23:51 +0800

    feat: 增加history

--- src/components/Image.tsx
+++
@@ -1,8 +0,0 @@
-import { forwardRef, ImgHTMLAttributes } from 'react'
-import styled from 'styled-components'
-
-export const Image = forwardRef<HTMLImageElement, ImgHTMLAttributes<HTMLImageElement>>((props, ref) => (
-  <_Image alt={''} {...props} ref={ref} />
-))
-
-const _Image = styled.img``
